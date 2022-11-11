33ec0520162d0e450007c54e39445d99da9c7f6b
Sayed Ghulam Murtaza <36192658+gmsha-tech@users.noreply.github.com>
2022-11-11 15:37:03 +0500

    Animations implementation and heart wave

--- app/src/main/java/com/gmshascreations/beautifulquran/PlayActvitiy.kt
+++ app/src/main/java/com/gmshascreations/beautifulquran/PlayActvitiy.kt
@@ -1,17 +1,27 @@
 package com.gmshascreations.beautifulquran
 
+import android.animation.ObjectAnimator
 import android.content.pm.ActivityInfo
 import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
 import android.view.Window
 import android.view.WindowManager
+import android.view.animation.AnimationUtils
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.skyfishjy.library.RippleBackground
+import com.yangp.ypwaveview.YPWaveView
 import java.util.*
 import kotlin.random.Random
 
 class PlayActvitiy : AppCompatActivity() {
     lateinit var mediaPlayer: MediaPlayer
+    lateinit var timer: Timer
+    lateinit var progress: YPWaveView
+    lateinit var useHeadphones: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // remove title
@@ -33,40 +43,53 @@ class PlayActvitiy : AppCompatActivity() {
         mediaPlayer = MediaPlayer.create(this, R.raw.quran_audio)
         mediaPlayer.start() // no need to call prepare(); create() does that for you
 
+        useHeadphones = findViewById(R.id.useHeaphones)
+        val  animationFadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in)
+        val  animationFadeOut = AnimationUtils.loadAnimation(this, R.anim.fade_out)
+        useHeadphones.visibility =View.VISIBLE
+
+        useHeadphones.startAnimation(animationFadeIn)
+        Handler().postDelayed({
+           useHeadphones.startAnimation(animationFadeOut)
+            useHeadphones.visibility =View.GONE
+        }, 3000)
+
+        progress = findViewById(R.id.progress)
+        progress.max = mediaPlayer.duration
+        timer = Timer()
+
+        timer.schedule(object : TimerTask() {
+            override fun run() {
+
+                progress.progress = mediaPlayer.currentPosition
+            }
+        }, 0, 1000)
     }
 
 
     override fun onDestroy() {
         super.onDestroy()
         mediaPlayer.release()
+        timer.cancel()
     }
 
     override fun onPause() {
         super.onPause()
         if (mediaPlayer.isPlaying)
             mediaPlayer.pause()
+        timer.cancel()
     }
 
     override fun onResume() {
         super.onResume()
         if (!mediaPlayer.isPlaying) {
             mediaPlayer.start()
+            timer.schedule(object : TimerTask() {
+                override fun run() {
+
+                    progress.progress = mediaPlayer.currentPosition
+                }
+            }, 0, 1000)
         }
     }
-
-    private fun createWaveform(): IntArray? {
-        val random = Random(System.currentTimeMillis())
-        val length = 50
-        val values = IntArray(length)
-        var maxValue = 0
-        for (i in 0 until length) {
-            val newValue: Int = 5 + random.nextInt(50)
-            if (newValue > maxValue) {
-                maxValue = newValue
-            }
-            values[i] = newValue
-        }
-        return values
-    }
-
 }
